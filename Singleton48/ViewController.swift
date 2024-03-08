b38be5c4d9c12fce87b5a780fcea3587c956bd95
aiturann <aiturgannnnnn@gmail.com>
2024-03-08 14:31:33 +0600

    Initial Commit

---
+++ Singleton48/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Singleton48
+//
+//  Created by Aiturgan Kurmanbekova on 8/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
